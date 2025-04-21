6633799acb11242140eb35b0086e2545c4c7922c
KimYohan <119276700+KimYohan0317@users.noreply.github.com>
2025-04-21 14:49:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from utils import calculate_metrics
 import logging
 from datetime import datetime
 
-# 경고 메시지 비활성화
 transformers_logging.set_verbosity_error()
 
 random.seed(0)
@@ -23,7 +22,7 @@ torch.manual_seed(0)
 def contrastive_model_train(model, trn_loader, tst_loader, device, classification_criterion, contrastive_criterion, optimizer, epochs, data_name, pooling_method, alpha):
     model.train()
     best_accuracy = 0.0
-    best_model_path = f"/dshome/ddualab/yohan/clr/best_model/{data_name}_best_mymodel_{pooling_method}.pth"
+    best_model_path = f"./best_model/{data_name}_best_mymodel_{pooling_method}.pth"
 
     for epoch in range(epochs):
         total_contrastive_loss = 0.0
@@ -67,7 +66,7 @@ def contrastive_model_train(model, trn_loader, tst_loader, device, classificatio
         if accuracy > best_accuracy:
             best_accuracy = accuracy
             torch.save(model.state_dict(), best_model_path)
-            msg = f"✅ Best model saved with accuracy: {best_accuracy:.4f}"
+            msg = f" Best model saved with accuracy: {best_accuracy:.4f}"
             print(msg)
             logging.info(msg)
     
@@ -114,15 +113,14 @@ def main():
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     num_epochs = 50
-    pooling_method = 'average' #attention or average
+    pooling_method = 'average'
     transform = transforms.Compose([
         transforms.Resize(img_size),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
     ])
-    #오늘 날짜 지정
     today = datetime.today().strftime('%Y-%m-%d-%H:%M')
-    log_dir = '/dshome/ddualab/yohan/clr/log'
+    log_dir = './logs'
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
@@ -134,22 +132,17 @@ def main():
         level=logging.INFO
     )
     
-    # 여러 데이터셋에 대한 반복
-    # data_names = ['Beef', 'Car', 'CBF', 'ChlorineConcentration', 'CinCECGTorso', 'Coffee', 'Computers', 'Earthquakes', 'ECG200', 'ECG5000', 'ECGFiveDays']
-    data_names = ['SonyAIBORobotSurface2']
-    # data_names = ['ElectricDevices', 'FordA', 'FordB']
-    # data_names = ['Ham', 'InsectWingbeatSound', 'ItalyPowerDemand', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7', 'Mallat', 'Meat', 'MoteStrain', 'NonInvasiveFetalECGThorax1']
-    # data_names = [ 'NonInvasiveFetalECGThorax2', 'OliveOil', 'Phoneme', 'Plane', 'RefrigerationDevices', 'ScreenType', 'ShapeletSim', 'SmallKitchenAppliances', 'SonyAIBORobotSurface1', 'SonyAIBORobotSurface2']
-    # data_names = [ 'StarLightCurves', 'Strawberry', 'SyntheticControl', 'Trace', 'TwoLeadECG', 'TwoPatterns', 'Wafer', 'Wine', 'ACSF1']
-    # data_names = ['BME', 'Chinatown', 'DodgerLoopDay', 'DodgerLoopGame', 'DodgerLoopWeekend', 'EOGHorizontalSignal', 'EOGVerticalSignal', 'EthanolLevel']
-    # data_names = ['FreezerRegularTrain', 'FreezerSmallTrain', 'HouseTwenty', 'MelbournePedestrian', 'SmoothSubspace', 'UMD']
+    data_names = ['Beef', 'Car', 'CBF', 'ChlorineConcentration', 'CinCECGTorso', 'Coffee', 'Computers', 'Earthquakes', 'ECG200', 'ECG5000', 'ECGFiveDays']
+
     for data_name in data_names:
         logging.info(f"Starting training for dataset: {data_name}")
         print(f"Starting training for dataset: {data_name}")
         
-        # 모델 초기화
-        label_counts = len(pd.read_csv(f'/dshome/ddualab/yohan/clr/raw_dataset/{data_name}/{data_name}_train_df.csv').iloc[:, -3].unique())
-        input_shape = len(pd.read_csv(f'/dshome/ddualab/yohan/clr/raw_dataset/{data_name}/{data_name}_train_df.csv').iloc[:, :-4].columns)
+        train_path = f'./raw_dataset/{data_name}/{data_name}_train_df.csv'
+        test_path = f'./raw_dataset/{data_name}/{data_name}_test_df.csv'
+
+        label_counts = len(pd.read_csv(train_path).iloc[:, -3].unique())
+        input_shape = len(pd.read_csv(train_path).iloc[:, :-4].columns)
         model = nn.DataParallel(ViTTransformerModel(input_shape=input_shape, num_classes=label_counts, pooling_method=pooling_method, out_dim=out_dim, device=device)) 
         model.to(device)
         
@@ -157,12 +150,8 @@ def main():
         criterion = torch.nn.CrossEntropyLoss()
         contrastive_criterion = ContrastiveLoss(device, temperature)
         
-        # 데이터셋 로드
-        train_data_path = f'/dshome/ddualab/yohan/clr/raw_dataset/{data_name}/{data_name}_train_df.csv'
-        test_data_path = f'/dshome/ddualab/yohan/clr/raw_dataset/{data_name}/{data_name}_test_df.csv'
-        
-        train_df = pd.read_csv(train_data_path)
-        test_df = pd.read_csv(test_data_path)
+        train_df = pd.read_csv(train_path)
+        test_df = pd.read_csv(test_path)
         
         train_dataset = ClrDataset(train_df, transform=transform)
         train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True, drop_last=True)
@@ -170,8 +159,7 @@ def main():
         test_dataset = ClrDataset(test_df, transform=transform)
         test_dataloader = DataLoader(test_dataset, batch_size, shuffle=False, drop_last=True)
         
-        # 학습 수행
         contrastive_model_train(model, train_dataloader, test_dataloader, device, criterion, contrastive_criterion, optimizer, num_epochs, data_name, pooling_method, alpha)
-
+    
 if __name__ == '__main__':
     main()
